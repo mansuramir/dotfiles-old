77f2c592a71939343bcb8d0420ee9512b33ced51
Mansuriatus Shahrir Amir <mansuramir@pd.jaring.my>
2017-12-20 12:09:12 +0700

    2017-12-20 Add fish/config.fish

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+set -x  GOROOT /usr/local/opt/go/libexec
+set -x GOPATH /Users/mansur/Code/go
+set -x PATH $PATH $GOROOT/bin $GOPATH/bin
+set -x PATH $PATH /Users/mansur/.fzf/bin
+
+set -x NODE_PATH /Users/mansur/Code/node_modules $NODE_PATH
+
+set -x PYTHON /usr/local/bin/python3
